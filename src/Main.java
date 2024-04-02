35d25d3062f488390ed72a59a8d644524f7b52bd
NScott65 <144457750+NScott65@users.noreply.github.com>
2024-04-02 09:42:01 -0400

    h

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashMap<String, Integer> grades = new HashMap<String, Integer>();
+
+        grades.put("Engineering Design", 100);
+        grades.put("Physics", 95);
+        grades.put("PreCalc", 91);
+        grades.put("Programming", 102);
+        grades.put("Comp II", 92);
+
+        System.out.println("Grades By Subject:\n");
+
+
+
     }
 }
